ea71ee35a817fc4e849e98dcaeb427344e9122f3
mn1272 <108688673+mn1272@users.noreply.github.com>
2022-08-08 16:42:31 -0400

    open webpage through qr code

--- code.py
+++ code.py
@@ -1,7 +1,10 @@
 import cv2
 from pyzbar import pyzbar
+import webbrowser
+import platform
 
 def read_barcodes(frame):
+    barcode_info = ''
     barcodes = pyzbar.decode(frame)
     for barcode in barcodes:
         x, y , w, h = barcode.rect
@@ -12,19 +15,39 @@ def read_barcodes(frame):
         cv2.putText(frame, barcode_info, (x + 6, y - 6), font, 2.0, (255, 255, 255), 1)
         with open("barcode_result.txt", mode ='w') as file:
             file.write("Recognized Barcode:" + barcode_info)
-    return frame
+    return frame, barcode_info
 
 
 def main():
     camera = cv2.VideoCapture(0)
     ret, frame = camera.read()
+
+    if platform.system().lower() == "linux" or platform.system().lower() == "linux2":
+        # Linux
+        browser_path = '/usr/bin/google-chrome %s'
+    elif platform.system().lower() == "darwin":          
+        # MacOS
+        browser_path = 'open -a /Applications/Google\ Chrome.app %s'
+    elif platform.system().lower() == "win32":             
+        # Windows
+        browser_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
+    else:
+        print('Could not detect OS')
+        exit
+    
     while ret:
         ret, frame = camera.read()
-        frame = read_barcode(frame)
+        frame, barcode_info = read_barcodes(frame)
         cv2.imshow('Barcode/QR code reader', frame)
+        if barcode_info:
+            print(barcode_info)
+            webbrowser.get(browser_path).open(barcode_info)
+            break
         if cv2.waitKey(1) & 0xFF == 27:
             break
+        
     camera.release()
     cv2.destroyAllWindows()
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
